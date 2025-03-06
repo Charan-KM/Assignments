63ff940b788fdc6c86a48b762e38c150824cf5ff
Charan-KM <charan@volopay.co>
2025-03-06 11:57:45 +0530

    Added page route in App.jsx and added BrowserRoute to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import TablePage from "./pages/table";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <TablePage />;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TablePage />} />
+    </Routes>
+  );
+};
 
 export default App;
