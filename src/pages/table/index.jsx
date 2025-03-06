26edc9338b559ae53d548f6e87f5e29236295dce
Charan-KM <charan@volopay.co>
2025-03-06 11:47:42 +0530

    Edited constants and index.jsx

--- src/pages/table/index.jsx
+++ src/pages/table/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import TableData from "../../components/core/TableData";
+import TopGainersTable from "../../components/table/TopGainersTable";
+import TopLosersTable from "../../components/table/TopLosersTable";
 
 const TablePage = () => {
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">Dynamic Tables</h1>
-      <TableData />
+      <h1 className="text-2xl font-bold mb-6">Stock Table Pages</h1>
+      <div className="grid md:grid-cols-2 gap-6">
+        <TopGainersTable />
+        <TopLosersTable />
+      </div>
     </div>
   );
 };
