7a7690f8e189c7a937366ccb20e27b3e1a13cce5
Charan-KM <charan@volopay.co>
2025-03-07 19:02:42 +0530

    Updated main.jsx and deleted app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/reducers/store.js";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StockTablePage from "./pages/stocks/StockTablePage";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </BrowserRouter>
+      <StrictMode>
+        <Routes>
+          <Route path="/" element={<StockTablePage />} />
+        </Routes>
+      </StrictMode>
+    </BrowserRouter>
   </Provider>
-)
+);
