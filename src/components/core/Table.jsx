dae7c3501a7b279a172801c62f65d7881b4e0552
Charan-KM <charan@volopay.co>
2025-03-17 14:49:57 +0530

    Tweaked Table core component

--- src/components/core/Table.jsx
+++ src/components/core/Table.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 import { HEADER_LABELS } from "../../constants/stocks";
 import { Link } from "react-router-dom";
 
-const TableComponent = ({ headers, data }) => {
+const TableComponent = ({ headers, data, lastItemRef }) => {
+    console.log("Formatted Data:", data);
+    console.log("Headers Passed to Table:", headers);
+
     if (!headers?.length || !data?.length) {
-        return <div>Data Unavailable</div>;
+        return <div className="text-center p-4">Data Unavailable</div>;
     }
 
     return (
@@ -15,21 +18,34 @@ const TableComponent = ({ headers, data }) => {
                     <tr>
                         {headers.map((header, index) => (
                             <th key={index} className="p-3 text-left border-b-2 border-gray-200 bg-gray-100 font-semibold">
-                                {HEADER_LABELS[header] || header.toUpperCase()}
+                                {HEADER_LABELS[header] || header}
                             </th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((row, rowIndex) => (
-                        <tr key={rowIndex} className="cursor-pointer hover:bg-gray-200">
-                            {headers.map((header, colIndex) => (
-                                <td key={colIndex} className="p-3 border-b border-gray-200 text-left">
-                                    <Link to={`/company/${row.ticker}`} className="block w-full">
-                                        {row[header] || "-"}
-                                    </Link>
-                                </td>
-                            ))}
+                        <tr 
+                            key={rowIndex} 
+                            ref={rowIndex === data.length - 1 ? lastItemRef : null}
+                            className="cursor-pointer hover:bg-gray-200"
+                        >
+                            {headers.map((header, colIndex) => {
+                                if (header === "ref") return null;
+                                
+                                const cellData = row[header] || "";
+                                return (
+                                    <td key={colIndex} className="p-3 border-b border-gray-200 text-left">
+                                        {row.ticker ? (
+                                            <Link to={`/company/${row.ticker}`} className="block w-full">
+                                                {cellData}
+                                            </Link>
+                                        ) : (
+                                            cellData
+                                        )}
+                                    </td>
+                                );
+                            })}
                         </tr>
                     ))}
                 </tbody>
@@ -41,6 +57,7 @@ const TableComponent = ({ headers, data }) => {
 TableComponent.propTypes = {
     headers: PropTypes.arrayOf(PropTypes.string).isRequired,
     data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    lastItemRef: PropTypes.func
 };
 
 export default TableComponent;
